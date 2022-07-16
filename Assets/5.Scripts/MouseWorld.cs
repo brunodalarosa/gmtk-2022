619ce5e8c707f6b193667ada09c6497172d31191
Bruno Cesar Puli Dala Rosa <obrunodalarosa@gmail.com>
2022-07-16 11:14:57 -0300

    Mouse World Plane Stuff

---
+++ Assets/5.Scripts/MouseWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace _5.Scripts
+{
+    public class MouseWorld : MonoBehaviour
+    {
+        private static MouseWorld instance;
+
+        [field: SerializeField] private LayerMask PlaneLayerMask;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Update()
+        {
+            transform.position = MouseWorld.GetPosition();
+        }
+
+        public static Vector3 GetPosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.PlaneLayerMask);
+            return raycastHit.point;
+        }
+    }
+}
