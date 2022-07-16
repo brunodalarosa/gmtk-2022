ad289532a099a21be0f496ca182e244fadcc1fda
edu-fr <edu.felipe1@outlook.com>
2022-07-16 13:17:14 -0300

    enemy-creation-and-script-subdirectiores

---
+++ Assets/5.Scripts/EnemySpawner.cs
@@ -0,0 +1,13 @@
+using Global;
+using UnityEngine;
+
+namespace _5.Scripts
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private PlayerMovement player;
+        
+        
+        
+    }
+}
