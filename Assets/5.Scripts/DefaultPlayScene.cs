0f43054bc4c93a8df74d316a1c08fc3940c1faba
Bruno Cesar Puli Dala Rosa <obrunodalarosa@gmail.com>
2022-07-17 05:17:28 -0300

    OBRIGA O JOGO A RODAR DO MENU

---
+++ Assets/5.Scripts/DefaultPlayScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace _5.Scripts
+{
+    public static class DefaultPlayScene
+    {
+        [InitializeOnLoadMethod]
+        private static void SetDefaultPlayScene()
+        {
+            if (EditorBuildSettings.scenes.Length == 0) return;
+            EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+        }
+    }
+}
